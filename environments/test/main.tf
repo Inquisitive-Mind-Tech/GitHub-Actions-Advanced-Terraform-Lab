79f395c92b25bd9c6ab527700ba8647e9345e4f5
Saurav Tomer <sauravtomer.dakshana17@gmail.com>
2026-08-24 19:34:10 +0530

    Corrected source path in main.tf inside test env

--- environments/test/main.tf
+++ environments/test/main.tf
@@ -1,10 +1,10 @@
 module "RG" {
-  source = "../../Child_Modules/1_Resource-Group"
+  source = "../../modules/1_azurerm_resource_group"
   c_rg   = var.p_rg
 }
 
 module "StAcc" {
-  source     = "../../Child_Modules/2_Storage-Account"
+  source     = "../../modules/2_azurerm_storage_account"
   depends_on = [module.RG]
   c_sa       = var.p_sa
 }
